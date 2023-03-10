8397a5dae938d67ba725cab39b64398ca0f7b0e1
wandakim <nwd1583@naver.com>
2023-03-11 04:37:35 +0900

    done

--- src/main.js
+++ src/main.js
@@ -1,29 +1,49 @@
+// Fetch the items from the JSON file
 function loadItems() {
   return fetch('data/data.json')
     .then((response) => response.json())
     .then((json) => json.items);
 }
 
+// Update the list with the given items
 function displayItems(items) {
   const container = document.querySelector('.items');
-  container.innerHTML = items.map((item) => createHTMLString(item));
+  container.innerHTML = items.map((item) => createHTMLString(item)).join('');
 }
-// create HTML list item from the given data item
+
+// Create HTML list item from the given data item
 function createHTMLString(item) {
   return `
     <li class="item">
-      <img src="${item.image}" alt="${item.type}" class="item__thumbnail" />
-      <span class="item__description">${item.gender}, ${item.size}</span>
-    </li>`;
+        <img src="${item.image}" alt="${item.type}" class="item__thumbnail" />
+        <span class="item__description">${item.gender}, ${item.size}</span>
+    </li>
+    `;
 }
 
-function setEventListeners(items) {}
+function onButtonClick(event, items) {
+  const dataset = event.target.dataset;
+  const key = dataset.key;
+  const value = dataset.value;
 
-//main
+  if (key == null || value == null) {
+    return;
+  }
+
+  displayItems(items.filter((item) => item[key] === value));
+}
+
+function setEventListeners(items) {
+  const logo = document.querySelector('.logo');
+  const buttons = document.querySelector('.buttons');
+  logo.addEventListener('click', () => displayItems(items));
+  buttons.addEventListener('click', (event) => onButtonClick(event, items));
+}
+
+// main
 loadItems()
   .then((items) => {
-    console.log(items);
     displayItems(items);
-    // setEventListensers(items)
+    setEventListeners(items);
   })
   .catch(console.log);
